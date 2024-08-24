f3697aa1355c3cff0331194aa2e782a96b300cc4
kony_ogony <45127834+Kony-Ogony@users.noreply.github.com>
2024-08-24 19:07:38 +0700

    snake_case

--- frontend/src/utils/cookies.rs
+++ frontend/src/utils/cookies.rs
@@ -1,8 +1,18 @@
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen(module = "/js/cookies.js")]
+#[wasm_bindgen(raw_module = "../../js/cookies.js")]
 extern "C" {
-    pub fn setCookie(name: &str, value: &str, days: i32);
-    pub fn getCookie(name: &str) -> JsValue;
-    pub fn eraseCookie(name: &str);
+    fn setCookie(name: &str, value: &str, days: i32);
+    fn getCookie(name: &str) -> JsValue;
+    fn eraseCookie(name: &str);
+}
+
+pub fn set_cookie(name: &str, value: &str, days: i32) {
+    setCookie(name, value, days)
+}
+pub fn get_cookie(name: &str) -> JsValue {
+    getCookie(name)
+}
+pub fn erase_cookie(name: &str) {
+    eraseCookie(name)
 }
