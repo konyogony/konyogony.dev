20a27c10bb79b5eb29073fac1fa74bdd048eb2ba
konyogony <45127834+konyogony@users.noreply.github.com>
2024-09-11 21:02:50 +0700

    Add dark theme

--- frontend/src/layouts/layout.tsx
+++ frontend/src/layouts/layout.tsx
@@ -1,12 +1,15 @@
 import { Navbar } from '@/components/custom/navbar';
+import { ThemeProvider } from '@/components/ui/theme-provider';
 import { Toaster } from '@/components/ui/toaster';
 
 export const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div className='felx mx-[20%] mt-4 min-h-screen w-full flex-col gap-2 bg-background'>
-            <Navbar />
-            {children}
-            <Toaster />
-        </div>
+        <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+            <div className='flex min-h-screen w-full flex-col gap-2 bg-zinc-950 pt-12'>
+                <Navbar />
+                <div className='flex h-full w-full flex-shrink-0 flex-col items-center px-[25%]'>{children}</div>
+                <Toaster />
+            </div>
+        </ThemeProvider>
     );
 };
