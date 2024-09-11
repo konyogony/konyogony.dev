02b9d61d1d0640246f525a99b564dfd92a28876f
konyogony <45127834+konyogony@users.noreply.github.com>
2024-09-11 21:03:07 +0700

    Make social redirects

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,12 +1,55 @@
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, useParams } from 'react-router-dom';
+import { Layout } from './layouts/layout';
+import { Notes } from './pages/notes';
+import { NotFound } from './pages/notfound';
 import { Welcome } from './pages/welcome';
 
 export const App = () => {
+    const ProtectedRoute = ({ component }: { component: JSX.Element }) => {
+        console.log('Protected route');
+        return component;
+    };
+
+    const RedirectSocial = () => {
+        const { service } = useParams<{ service: string }>();
+        switch (service) {
+            case 'github':
+                window.location.href = 'https://github.com/konyogony';
+                break;
+            case 'discord':
+                window.location.href = 'https://discordlookup.com/user/564472732071493633/';
+                break;
+            case 'spotify':
+                window.location.href = 'https://open.spotify.com/user/xeq03n90tcwkg4tegzdxggvzd/';
+                break;
+            case 'steam':
+                window.location.href = 'https://steamcommunity.com/id/kony_ogony/';
+                break;
+            default:
+                return <NotFound />;
+        }
+        return null;
+    };
+
     return (
-        <HashRouter>
-            <Routes>
-                <Route path='/' element={<Welcome />} />
-            </Routes>
-        </HashRouter>
+        <BrowserRouter>
+            <Layout>
+                <Routes>
+                    {/* Public routes */}
+                    <Route path='/' element={<Welcome />} />
+                    <Route path='/welcome' element={<Welcome />} />
+
+                    {/* Protected routes */}
+                    <Route path='/notes' element={<ProtectedRoute component={<Notes />} />} />
+
+                    {/* Social redirect */}
+                    <Route path='/social/:service' element={<RedirectSocial />} />
+
+                    {/* Not found */}
+                    <Route path='*' element={<NotFound />} />
+                    <Route path='404' element={<NotFound />} />
+                </Routes>
+            </Layout>
+        </BrowserRouter>
     );
 };
