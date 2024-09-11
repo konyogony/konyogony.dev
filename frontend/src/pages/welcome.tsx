632f9518e3828eb21d1bb136d914ef911122c47e
konyogony <45127834+konyogony@users.noreply.github.com>
2024-09-11 21:03:19 +0700

    Add future pages

--- frontend/src/pages/welcome.tsx
+++ frontend/src/pages/welcome.tsx
@@ -3,9 +3,13 @@ import { Layout } from '@/layouts/layout';
 
 export const Welcome = () => {
     return (
-        <Layout>
-            <h1>App</h1>
-            <Button>Cool button</Button>
-        </Layout>
+        <>
+            <div className='flex h-[25vh] flex-col items-center justify-center gap-1'>
+                <span className='text-6xl font-bold tracking-wide'>Meet kony_ogony,</span>
+                <span className='text-xl font-medium'>a junior full-stack developer, who does cool stuff!</span>
+            </div>
+            {/* <Button>Cool button</Button> */}
+            <div className='h-[200rem]'></div>
+        </>
     );
 };
