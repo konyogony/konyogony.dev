38bcdacc3ae27d43bc00f4af9dc65899c2a1084e
konyogony <45127834+konyogony@users.noreply.github.com>
2024-09-12 19:04:45 +0700

    Make hero look good

--- frontend/src/pages/welcome.tsx
+++ frontend/src/pages/welcome.tsx
@@ -1,15 +1,69 @@
+import { BackgroundBeams } from '@/components/ui/background-beams';
 import { Button } from '@/components/ui/button';
-import { Layout } from '@/layouts/layout';
+import { FiArrowRight } from 'react-icons/fi';
+import { RxMagnifyingGlass } from 'react-icons/rx';
+import {
+    SiActix,
+    SiGithub,
+    SiReact,
+    SiRust,
+    SiShadcnui,
+    SiSurrealdb,
+    SiTailwindcss,
+    SiTypescript,
+    SiVisualstudiocode,
+    SiVite,
+} from 'react-icons/si';
 
 export const Welcome = () => {
     return (
         <>
-            <div className='flex h-[25vh] flex-col items-center justify-center gap-1'>
-                <span className='text-6xl font-bold tracking-wide'>Meet kony_ogony,</span>
-                <span className='text-xl font-medium'>a junior full-stack developer, who does cool stuff!</span>
+            <div className='relative flex h-[75vh] w-full flex-col items-center justify-center gap-8 border-b border-white/5'>
+                <div className='z-40 flex flex-col items-center gap-1 bg-gradient-to-t from-zinc-400 to-zinc-200 bg-clip-text text-transparent'>
+                    <span className='text-7xl font-bold tracking-wide'>Meet kony_ogony,</span>
+                    <span className='text-xl font-medium'>a junior full-stack developer, who does cool stuff!</span>
+                </div>
+                <div className='z-40 flex flex-row items-center gap-4'>
+                    <Button className='flex flex-row items-center gap-1'>
+                        <span>Contact me</span> <FiArrowRight />
+                    </Button>
+                    <Button className='flex flex-row items-center gap-1' variant={'secondary'}>
+                        <span>Learn more</span> <RxMagnifyingGlass size={16} />
+                    </Button>
+                </div>
+                <div className='z-40 flex flex-row items-center gap-4'>
+                    <a href='https://ui.shadcn.com/' target='_blank' rel='noreferrer'>
+                        <SiShadcnui />
+                    </a>
+                    <a href='https://react.dev/' target='_blank' rel='noreferrer'>
+                        <SiReact size={20} />
+                    </a>
+                    <a href='https://tailwindcss.com/' target='_blank' rel='noreferrer'>
+                        <SiTailwindcss size={20} />
+                    </a>
+                    <a href='https://www.typescriptlang.org/' target='_blank' rel='noreferrer'>
+                        <SiTypescript size={20} />
+                    </a>
+                    <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
+                        <SiVite size={20} />
+                    </a>
+                    <a href='https://actix.rs/' target='_blank' rel='noreferrer'>
+                        <SiActix size={20} />
+                    </a>
+                    <a href='https://www.rust-lang.org' target='_blank' rel='noreferrer'>
+                        <SiRust size={20} />
+                    </a>
+                    <a href='https://surrealdb.com/' target='_blank' rel='noreferrer'>
+                        <SiSurrealdb size={20} />
+                    </a>
+                </div>
+                <BackgroundBeams className='hidden motion-reduce:hidden lg:flex' />
             </div>
-            {/* <Button>Cool button</Button> */}
-            <div className='h-[200rem]'></div>
+            <div className='flex flex-col items-center gap-2'>
+                <span className='text-2xl font-semibold'>Some of my work</span>
+            </div>
+
+            <div className='h-[200rem]' />
         </>
     );
 };
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,10 +1,27 @@
+import svgToDataUri from 'mini-svg-data-uri';
 import type { Config } from 'tailwindcss';
 import animate from 'tailwindcss-animate';
+import colors from 'tailwindcss/colors';
+import { default as flattenColorPalette } from 'tailwindcss/lib/util/flattenColorPalette';
 
 export default {
-    darkMode: ['class'],
+    darkMode: 'class',
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+
     theme: {
+        animation: {
+            shimmer: 'shimmer 2s linear infinite',
+        },
+        keyframes: {
+            shimmer: {
+                from: {
+                    backgroundPosition: '0 0',
+                },
+                to: {
+                    backgroundPosition: '-200% 0',
+                },
+            },
+        },
         extend: {
             borderRadius: {
                 lg: 'var(--radius)',
@@ -14,5 +31,37 @@ export default {
             colors: {},
         },
     },
-    plugins: [animate],
+    plugins: [animate, addVariablesForColors, grid],
 } satisfies Config;
+
+function addVariablesForColors({ addBase, theme }: any) {
+    let allColors = flattenColorPalette(theme('colors'));
+    let newVars = Object.fromEntries(Object.entries(allColors).map(([key, val]) => [`--${key}`, val]));
+
+    addBase({
+        ':root': newVars,
+    });
+}
+
+function grid({ matchUtilities, theme }: any) {
+    matchUtilities(
+        {
+            'bg-grid': (value: any) => ({
+                backgroundImage: `url("${svgToDataUri(
+                    `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`,
+                )}")`,
+            }),
+            'bg-grid-small': (value: any) => ({
+                backgroundImage: `url("${svgToDataUri(
+                    `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`,
+                )}")`,
+            }),
+            'bg-dot': (value: any) => ({
+                backgroundImage: `url("${svgToDataUri(
+                    `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`,
+                )}")`,
+            }),
+        },
+        { values: flattenColorPalette(theme('backgroundColor')), type: 'color' },
+    );
+}
