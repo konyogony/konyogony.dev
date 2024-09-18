64f170b83cb26745c076ab13d94c8b35ea845904
konyogony <45127834+konyogony@users.noreply.github.com>
2024-09-18 19:34:50 +0700

    Small features & fixes

--- frontend/src/layouts/layout.tsx
+++ frontend/src/layouts/layout.tsx
@@ -8,7 +8,7 @@ export const Layout = ({ children }: { children: React.ReactNode }) => {
             <div className='flex min-h-screen w-full flex-col gap-2 bg-zinc-950'>
                 <Navbar />
                 <div className='flex h-full w-full flex-shrink-0 flex-col items-center'>{children}</div>
-                <Toaster theme={'dark'} />
+                <Toaster theme={'dark'} richColors />
             </div>
         </ThemeProvider>
     );
