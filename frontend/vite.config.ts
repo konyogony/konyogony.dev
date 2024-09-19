84d4a27bac694d178176d4c27253d121f4b89c33
konyogony <45127834+konyogony@users.noreply.github.com>
2024-09-19 21:39:52 +0700

    Make accessible on LAN

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,20 +3,21 @@ import react from '@vitejs/plugin-react-swc';
 import remarkGfm from 'remark-gfm';
 import { defineConfig } from 'vite';
 
-export default defineConfig(() => {
-    return {
-        plugins: [
-            mdx({
-                providerImportSource: '@mdx-js/react',
-                remarkPlugins: [remarkGfm],
-            }),
-            react(),
-        ],
-        resolve: {
-            alias: { '@': '/src' },
-        },
-        optimizeDeps: {
-            include: ['react/jsx-runtime'],
-        },
-    };
-});
+export default defineConfig(() => ({
+    server: {
+        host: '0.0.0.0',
+    },
+    plugins: [
+        mdx({
+            providerImportSource: '@mdx-js/react',
+            remarkPlugins: [remarkGfm],
+        }),
+        react(),
+    ],
+    resolve: {
+        alias: { '@': '/src' },
+    },
+    optimizeDeps: {
+        include: ['react/jsx-runtime'],
+    },
+}));
