b6f720251ce96c1c2dac23f14f10e4e82ce3942c
konyogony <45127834+konyogony@users.noreply.github.com>
2024-09-19 21:40:45 +0700

    QOL Changes to the code block

--- frontend/src/components/custom/codeWrapper.tsx
+++ frontend/src/components/custom/codeWrapper.tsx
@@ -1,41 +1,42 @@
 import { getHighlighter } from '@/lib/highlighterSingleton';
-import { FiClipboard } from '@vertisanpro/react-icons/fi';
+import { IconType } from '@vertisanpro/react-icons';
+import { FiClipboard, FiLoader } from '@vertisanpro/react-icons/fi';
 import { SiCss3, SiJavascript, SiMdx, SiReact, SiRust, SiTailwindcss, SiTypescript } from '@vertisanpro/react-icons/si';
 import { VscJson, VscSymbolFile, VscTerminal } from '@vertisanpro/react-icons/vsc';
 import copy from 'copy-to-clipboard';
 import { ReactNode, useEffect, useState } from 'react';
 import { toast } from 'sonner';
 
-const SelectIcon = ({ language }: { language: string }) => {
-    switch (language) {
-        case 'ts':
-            return <SiTypescript size={16} />;
-        case 'tsx':
-            return <SiReact size={16} />;
-        case 'js':
-            return <SiJavascript size={16} />;
-        case 'jsx':
-            return <SiReact size={16} />;
-        case 'rs':
-            return <SiRust size={16} />;
-        case 'html':
-            return <SiTailwindcss size={16} />;
-        case 'mdx':
-            return <SiMdx size={16} />;
-        case 'css':
-            return <SiCss3 size={16} />;
-        case 'json':
-            return <VscJson size={16} />;
-        case 'bash':
-        case 'sh':
-            return <VscTerminal size={16} />;
+const SelectIcon = ({ language }: { language: string }): { Icon: IconType; lang: string } => {
+    switch (language.toUpperCase()) {
+        case 'TS':
+            return { Icon: SiTypescript, lang: 'Typescript' } as const;
+        case 'JSX':
+        case 'TSX':
+            return { Icon: SiTypescript, lang: 'React' } as const;
+        case 'JS':
+            return { Icon: SiJavascript, lang: 'Javascript' } as const;
+        case 'RS':
+            return { Icon: SiRust, lang: 'Rust' } as const;
+        case 'HTML':
+            return { Icon: SiTailwindcss, lang: 'HTML' } as const;
+        case 'MDX':
+            return { Icon: SiMdx, lang: 'MDX' } as const;
+        case 'CSS':
+            return { Icon: SiCss3, lang: 'CSS' } as const;
+        case 'JSON':
+            return { Icon: VscJson, lang: 'JSON' } as const;
+        case 'BASH':
+        case 'SH':
+            return { Icon: VscTerminal, lang: 'Terminal' } as const;
         default:
-            return <VscSymbolFile size={16} />;
+            return { Icon: VscSymbolFile, lang: 'File' } as const;
     }
 };
 
 export const CodeWrapper = ({ language = '', children }: { language: string; children: ReactNode }) => {
     const [codeBlock, setCodeBlock] = useState<string>('');
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const fetchHighlighter = async () => {
@@ -45,18 +46,31 @@ export const CodeWrapper = ({ language = '', children }: { language: string; chi
                 theme: 'github-dark-dimmed',
             });
             setCodeBlock(highlightedCode);
+            setLoading(false);
         };
 
         fetchHighlighter();
     }, [language, children]);
 
+    const { Icon, lang } = SelectIcon({ language });
+
     return (
         <div className='group relative overflow-clip rounded-lg border border-white/15'>
             <div className='flex w-full flex-row items-center gap-1 border-b border-white/15 bg-[#1a1e24] px-4 py-2.5 text-sm font-normal text-zinc-200'>
-                <SelectIcon language={language} />
-                {language} <CopyButton text={children?.toString() || ''} />
+                <Icon size={16} />
+                {lang} <CopyButton text={children?.toString() || ''} />
             </div>
-            <article dangerouslySetInnerHTML={{ __html: codeBlock }} className='bg-zinc-950' id={'codeBlock'} />
+            {loading ? (
+                <div className='flex h-[20vh] w-full items-center justify-center bg-[#22272e]'>
+                    <FiLoader size={18} className='animate-spin-slow' />
+                </div>
+            ) : (
+                <article
+                    dangerouslySetInnerHTML={{ __html: codeBlock }}
+                    className='bg-zinc-950 text-sm lg:text-base'
+                    id={'codeBlock'}
+                />
+            )}
         </div>
     );
 };
@@ -68,7 +82,10 @@ const CopyButton: React.FC<{ text: string }> = ({ text }) => {
     };
 
     return (
-        <button onClick={clickCopy} className='ml-auto'>
+        <button
+            onClick={clickCopy}
+            className='ml-auto text-zinc-400 opacity-0 transition-all duration-150 hover:!text-zinc-200 group-hover:opacity-100'
+        >
             <FiClipboard size={18} />
         </button>
     );
