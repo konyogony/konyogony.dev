bab665a26e2dc63c5d677f73a5b3bdc1202ab65d
konyogony <45127834+konyogony@users.noreply.github.com>
2024-09-19 19:30:21 +0700

    Better CodeWrapper

--- frontend/src/components/custom/codeWrapper.tsx
+++ frontend/src/components/custom/codeWrapper.tsx
@@ -1,7 +1,7 @@
 import { getHighlighter } from '@/lib/highlighterSingleton';
+import { FiClipboard } from '@vertisanpro/react-icons/fi';
 import copy from 'copy-to-clipboard';
 import { ReactNode, useEffect, useState } from 'react';
-import { FiClipboard } from '@vertisanpro/react-icons/fi';
 import { toast } from 'sonner';
 
 export const CodeWrapper = ({ language = '', children }: { language: string; children: ReactNode }) => {
@@ -21,9 +21,11 @@ export const CodeWrapper = ({ language = '', children }: { language: string; chi
     }, [language, children]);
 
     return (
-        <div className='group relative rounded-lg'>
-            <div dangerouslySetInnerHTML={{ __html: codeBlock }} className='w-full' />
-            <CopyButton text={children?.toString() || ''} />
+        <div className='group relative overflow-clip rounded-lg border border-white/15'>
+            <div className='flex w-full flex-row items-center border-b border-white/15 bg-[#1a1e24] px-4 py-1 text-sm font-normal text-zinc-200'>
+                {language} <CopyButton text={children?.toString() || ''} />
+            </div>
+            <article dangerouslySetInnerHTML={{ __html: codeBlock }} className='bg-zinc-950' id={'codeBlock'} />
         </div>
     );
 };
@@ -35,10 +37,7 @@ const CopyButton: React.FC<{ text: string }> = ({ text }) => {
     };
 
     return (
-        <button
-            onClick={clickCopy}
-            className='absolute right-2 top-8 rounded-lg bg-zinc-500/20 opacity-0 transition-all duration-300 group-hover:opacity-100'
-        >
+        <button onClick={clickCopy} className='ml-auto rounded-lg'>
             <FiClipboard size={18} className='m-2' />
         </button>
     );
