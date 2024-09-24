f87dbe1f29fcf3def6c1c0087c3875c5fd60838f
konyogony <45127834+konyogony@users.noreply.github.com>
2024-09-24 21:06:59 +0700

    Optimizations???

--- frontend/src/components/wiki/wikiSecondarySidebar.tsx
+++ frontend/src/components/wiki/wikiSecondarySidebar.tsx
@@ -2,7 +2,7 @@ import { wikiConfig } from '@/config';
 import { cn } from '@/lib/utils';
 import { FileInfo } from '@/types';
 import { FiArrowUp, FiArrowUpRight } from '@vertisanpro/react-icons/fi';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 
 interface WikiSecondarySidebarProps {
     headings: (string | null)[];
@@ -23,25 +23,31 @@ export const WikiSecondarySidebar = ({ headings, currentIndex, structure }: Wiki
         return () => window.removeEventListener('scroll', handleScroll);
     }, [scrollHeight]);
 
-    return (
-        <div className='sticky top-24 hidden h-fit w-fit min-w-[20vh] flex-shrink-0 flex-col items-end lg:flex'>
-            <span className='-ml-1 py-2 text-sm font-bold text-zinc-50'>On this page</span>
-            {headings.map((v, i) => (
-                <React.Fragment key={i}>
-                    {v ? (
-                        <a
-                            href={`#${v.trim().toLocaleLowerCase().replaceAll(' ', '-') || v}`}
-                            className={cn(
-                                'py-1 text-sm font-light text-zinc-400 hover:text-zinc-200',
-                                // activeHeading === v.toLowerCase().replace(/\s+/g, '-') && 'font-bold text-zinc-50',
-                            )}
-                        >
-                            {v}
-                        </a>
-                    ) : null}
-                </React.Fragment>
-            ))}
-            <div className='my-2 h-[1px] w-3/4 bg-white/10' />
+    const headingsElement: JSX.Element = useMemo(
+        () => (
+            <>
+                {headings.map((v, i) => (
+                    <React.Fragment key={i}>
+                        {v ? (
+                            <a
+                                href={`#${v.trim().toLocaleLowerCase().replaceAll(' ', '-') || v}`}
+                                className={cn(
+                                    'py-1 text-sm font-light text-zinc-400 hover:text-zinc-200',
+                                    // activeHeading === v.toLowerCase().replace(/\s+/g, '-') && 'font-bold text-zinc-50',
+                                )}
+                            >
+                                {v}
+                            </a>
+                        ) : null}
+                    </React.Fragment>
+                ))}
+            </>
+        ),
+        [headings],
+    );
+
+    const editElement: JSX.Element = useMemo(
+        () => (
             <a
                 href={structure ? structure[currentIndex].gitPath : '/404'}
                 rel='noopener noreferrer'
@@ -50,18 +56,29 @@ export const WikiSecondarySidebar = ({ headings, currentIndex, structure }: Wiki
             >
                 Edit this page on GitHub <FiArrowUpRight />
             </a>
-            {scrollHeight > wikiConfig.scrollToTriggerButton && (
-                <>
-                    {headings.length > 0 && (
-                        <a
-                            href={`#${headings[0]?.trim().toLocaleLowerCase().replaceAll(' ', '-') || headings[0]}`}
-                            className='my-2 flex flex-row items-center gap-1 text-sm text-zinc-400 hover:text-zinc-200'
-                        >
-                            Back to top <FiArrowUp />
-                        </a>
-                    )}
-                </>
-            )}
+        ),
+        [structure, currentIndex],
+    );
+
+    const backToTopElement: JSX.Element = useMemo(
+        () => (
+            <a
+                href={`#${headings[0]?.trim().toLocaleLowerCase().replaceAll(' ', '-') || headings[0]}`}
+                className='my-2 flex flex-row items-center gap-1 text-sm text-zinc-400 hover:text-zinc-200'
+            >
+                Back to top <FiArrowUp />
+            </a>
+        ),
+        [headings],
+    );
+
+    return (
+        <div className='sticky top-24 hidden h-fit w-fit min-w-[20vh] flex-shrink-0 flex-col items-end lg:flex'>
+            <span className='-ml-1 py-2 text-sm font-bold text-zinc-50'>On this page</span>
+            {headingsElement}
+            <div className='my-2 h-[1px] w-3/4 bg-white/10' />
+            {editElement}
+            {scrollHeight > wikiConfig.scrollToTriggerButton && <>{backToTopElement}</>}
         </div>
     );
 };
