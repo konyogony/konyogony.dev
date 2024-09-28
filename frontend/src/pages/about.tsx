d2b0658f1bcd54258a8207f7ee885139b714f0c0
konyogony <45127834+konyogony@users.noreply.github.com>
2024-09-28 15:51:03 +0700

    Start doing charts, kinda broken

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -1,3 +1,13 @@
+import { Chart2 } from '@/components/custom/charts2';
+import { CustomChart } from '@/components/custom/customchart';
+import { MyChart } from '@/components/custom/mychart';
+
 export const About = () => {
-    return <>About</>;
+    return (
+        <>
+            <MyChart />
+            <Chart2 />
+            <CustomChart />
+        </>
+    );
 };
