cc34461abb2653a9f52c957b86423c7c837abcdf
konyogony <45127834+konyogony@users.noreply.github.com>
2024-10-03 20:42:22 +0700

    [frontend:ui] Made copy button display a tick when activated

--- frontend/src/components/custom/copyButton.tsx
+++ frontend/src/components/custom/copyButton.tsx
@@ -1,5 +1,7 @@
-import { FiClipboard } from '@vertisanpro/react-icons/fi';
+import { cn } from '@/lib/utils';
+import { FiCheck, FiClipboard } from '@vertisanpro/react-icons/fi';
 import copy from 'copy-to-clipboard';
+import { useState } from 'react';
 import { toast } from 'sonner';
 
 interface CopyButtonProps {
@@ -7,17 +9,35 @@ interface CopyButtonProps {
 }
 
 export const CopyButton: React.FC<CopyButtonProps> = ({ text }) => {
+    const [clicked, setClicked] = useState(false);
     const clickCopy = () => {
         copy(text);
         toast.success('Code copied to clipboard');
+        setClicked(true);
+        setTimeout(() => setClicked(false), 2000);
     };
 
     return (
         <button
             onClick={clickCopy}
-            className='ml-auto text-zinc-400 transition-all duration-150 hover:!text-zinc-200 lg:opacity-0 lg:group-hover:opacity-100'
+            className='text-zinc-400 transition-all duration-150 hover:!text-zinc-200 lg:opacity-0 lg:group-hover:opacity-100'
         >
-            <FiClipboard size={18} />
+            <FiCheck
+                size={18}
+                className={cn(
+                    'absolute right-2.5 top-2.5 transition-all duration-150',
+                    clicked ? 'text-emerald-500 opacity-100' : 'opacity-0',
+                )}
+            />
+            <FiClipboard
+                size={18}
+                className={cn(
+                    'absolute right-2.5 top-2.5 transition-all duration-150',
+                    !clicked ? 'opacity-100' : 'opacity-0',
+                )}
+            />
         </button>
     );
 };
+
+// Kinda ugly tbh
