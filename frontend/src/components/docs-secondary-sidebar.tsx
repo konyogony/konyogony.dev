67963427836046fff852af2386050570a6cce295
konyogony <45127834+konyogony@users.noreply.github.com>
2024-10-16 18:55:41 +0700

    Simple sidebar

--- frontend/src/components/docs-secondary-sidebar.tsx
+++ frontend/src/components/docs-secondary-sidebar.tsx
@@ -1,7 +1,37 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
 export const SecondarySidebar = () => {
+    const [headings, setHeadings] = useState<string[]>([]);
+    const pathname = usePathname();
+
+    useEffect(() => {
+        const headings = document.querySelectorAll('h1, h2, h3');
+        const headingText = Array.from(headings).map((heading) => heading.textContent || '');
+        setHeadings(headingText);
+    }, [pathname]);
+
     return (
         <div className='sticky top-24 hidden h-fit w-fit min-w-[20vh] flex-shrink-0 flex-col items-start lg:flex'>
             <span className='py-2 text-sm font-bold text-zinc-50'>On this page</span>
+            {headings.map((v, i) => (
+                <a
+                    href={`#${
+                        v
+                            .trim()
+                            .toLocaleLowerCase()
+                            .replace(/\s+/g, '-')
+                            .replace(/[^\p{L}\p{N}-]/gu, '')
+                            .replace(/\./g, '') || v
+                    }`}
+                    className={'py-1 text-sm font-light text-zinc-400 hover:text-zinc-200'}
+                    key={i}
+                >
+                    {v.length > 30 ? v.slice(0, 30) + '...' : v}
+                </a>
+            ))}
         </div>
     );
 };
